9b768ab5db459103345a070d6f59fa82521485da
Prarabdha <89309012+Prarabdha123@users.noreply.github.com>
2021-09-20 18:22:20 +0530

    Delete texttospeech1.py

--- texttospeech1.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-def test():
-    x = open("D:\workspace\codes\Texttospeech1.txt", "r+").read().replace("\n", " ")
-    y = "en"
-    z = gTTS(text = str(x), lang = y, slow = False)
-    z.save("Texttospeech1.mp3")
-test()
